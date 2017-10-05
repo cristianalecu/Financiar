53d52568bfc2f3e6a8d956bd969c2714f0a19684
Cristian Alecu <cristian.alecu@pss.ro>
2017-10-05 10:55:59 +0300

    process elif

--- django_cms/financiar/process_import_xml.py
+++ django_cms/financiar/process_import_xml.py
@@ -11,6 +11,13 @@ class SalesXmlProcessor():
     
     def __init__(self):
         self.locations = Location.objects.all()
+        self.channels = Channel.objects.all()
+        self.brands = Brand.objects.all()
+        self.categories = Category.objects.all()
+        self.subcategories = Subcategory.objects.all()
+        self.benchmarks = Benchmark.objects.all()
+        self.sales_concepts = SalesConcept.objects.all()
+        self.sales_concept_sizes = SalesConceptSize.objects.all()
         self.constnetworks = ConstNetwork.objects.all()
         
     def get_object_by_name(self, attr_name, objects, classname):
@@ -36,9 +43,6 @@ class SalesXmlProcessor():
             for Worksheet in root.iter('{urn:schemas-microsoft-com:office:spreadsheet}Worksheet'):
                 for Table in Worksheet.iter('{urn:schemas-microsoft-com:office:spreadsheet}Table'):
                     SalesData.objects.all().delete()
-                    locations = Location.objects.all()
-                    Location.objects.all().delete()
-                    locations = Location.objects.all()
                     for Row in Table.iter('{urn:schemas-microsoft-com:office:spreadsheet}Row'):
                         if first_row :
                             first_row = 0
@@ -48,77 +52,31 @@ class SalesXmlProcessor():
                                 for Data in Cell.iter('{urn:schemas-microsoft-com:office:spreadsheet}Data'):
                                     table += " " + Data.tag # + " " + Cell.text # +  " " + Cell.attrib
                                     if coll == 3:
-                                        location = self.get_object_by_name(Data.text,locations, Location)
-                                    if coll == 4:
-                                        cn_vs_H = ConstNetwork.objects.filter(name=Data.text)
-                                        if cn_vs_H.count() == 0 :
-                                            cn_vs_H = ConstNetwork.objects.create(name=Data.text)
-                                            cn_vs_H.save()
-                                        else:
-                                            cn_vs_H = cn_vs_H[0]
-                                    if coll == 5:
-                                        cn_vs_B = ConstNetwork.objects.filter(name=Data.text)
-                                        if cn_vs_B.count() == 0 :
-                                            cn_vs_B = ConstNetwork.objects.create(name=Data.text)
-                                            cn_vs_B.save()
-                                        else:
-                                            cn_vs_B = cn_vs_B[0]
-                                    if coll == 6:
-                                        ebenchmark = Benchmark.objects.filter(name=Data.text)
-                                        if ebenchmark.count() == 0 :
-                                            ebenchmark = Benchmark.objects.create(name=Data.text)
-                                            ebenchmark.save()
-                                        else:
-                                            ebenchmark = ebenchmark[0]
-                                    if coll == 7:
-                                        bbenchmark = Benchmark.objects.filter(name=Data.text)
-                                        if bbenchmark.count() == 0 :
-                                            bbenchmark = Benchmark.objects.create(name=Data.text)
-                                            bbenchmark.save()
-                                        else:
-                                            bbenchmark = bbenchmark[0]
-                                    if coll == 8:
-                                        sales_concept = SalesConcept.objects.filter(name=Data.text)
-                                        if sales_concept.count() == 0 :
-                                            sales_concept = SalesConcept.objects.create(name=Data.text)
-                                            sales_concept.save()
-                                        else:
-                                            sales_concept = sales_concept[0]
-                                    if coll == 9:
-                                        sales_concept_size = SalesConceptSize.objects.filter(name=Data.text)
-                                        if sales_concept_size.count() == 0 :
-                                            sales_concept_size = SalesConceptSize.objects.create(name=Data.text)
-                                            sales_concept_size.save()
-                                        else:
-                                            sales_concept_size = sales_concept_size[0]
-                                    if coll == 10:
-                                        channel = Channel.objects.filter(name=Data.text)
-                                        if channel.count() == 0 :
-                                            channel = Channel.objects.create(name=Data.text)
-                                            channel.save()
-                                        else:
-                                            channel = channel[0]
-                                    if coll == 11:
-                                        brand = Brand.objects.filter(name=Data.text)
-                                        if brand.count() == 0 :
-                                            brand = Brand.objects.create(name=Data.text)
-                                            brand.save()
-                                        else:
-                                            brand = brand[0]
-                                    if coll == 13:
-                                        category = Category.objects.filter(name=Data.text)
-                                        if category.count() == 0 :
-                                            category = Category.objects.create(name=Data.text)
-                                            category.save()
-                                        else:
-                                            category = category[0]
-                                    if coll == 14:
-                                        subcategory = Subcategory.objects.filter(name=Data.text)
-                                        if subcategory.count() == 0 :
-                                            subcategory = Subcategory.objects.create(name=Data.text)
-                                            subcategory.save()
-                                        else:
-                                            subcategory = subcategory[0]
+                                        self.location = self.get_object_by_name(Data.text, self.locations, Location)
+                                    elif coll == 4:
+                                        self.cn_vs_H = self.get_object_by_name(Data.text, self.constnetworks, ConstNetwork)
+                                    elif coll == 5:
+                                        self.cn_vs_B = self.get_object_by_name(Data.text, self.constnetworks, ConstNetwork)
+                                    elif coll == 6:
+                                        self.ebenchmark = self.get_object_by_name(Data.text, self.benchmarks, Benchmark)
+                                    elif coll == 7:
+                                        self.bbenchmark = self.get_object_by_name(Data.text, self.benchmarks, Benchmark)
+                                    elif coll == 8:
+                                        self.sales_concept = self.get_object_by_name(Data.text, self.sales_concepts, SalesConcept)
+                                    elif coll == 9:
+                                        self.sales_concept_size = self.get_object_by_name(Data.text, self.sales_concept_sizes, SalesConceptSize)
+                                    elif coll == 10:
+                                        self.channel = self.get_object_by_name(Data.text, self.channels, Channel)
+                                    elif coll == 11:
+                                        self.brand = self.get_object_by_name(Data.text, self.brands, Brand)
+                                    elif coll == 13:
+                                        self.category = self.get_object_by_name(Data.text, self.categories, Category)
+                                    elif coll == 14:
+                                        self.subcategory = self.get_object_by_name(Data.text, self.subcategories, Subcategory)
+                                    elif coll == 15:
+                                        self.year=2016 
+                                        self.month = 1
+                                        self.insert()
                                 
                             sales = SalesData.objects.create()
                             sales.save()
